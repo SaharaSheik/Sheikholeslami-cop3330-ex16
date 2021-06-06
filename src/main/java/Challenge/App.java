676833d30f77a2cdeec8f76dc3ceedf85f469580
Sahar Sheikholeslami <sahara_sheik@hotmail.com>
2021-06-06 03:36:19 -0400

    Initial commit

---
+++ src/main/java/Challenge/App.java
@@ -0,0 +1,86 @@
+package Challenge;
+/*
+
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Sahar Sheikholeslami
+
+ */
+
+/*
+Program 16 - Legal Driving Age
+Challenge 1 - DONE!
+You can test for equality, but you may need to test to see how a number compares to a known value and display a message
+ if the number is too low or too high.
+
+Write a program that asks the user for their age and compare it to the legal driving age of sixteen. If the user
+is sixteen or older, then the program should display "You are old enough to legally drive." If the user is under
+sixteen, the program should display "You are not old enough to legally drive."
+
+Example Output
+What is your age? 15
+You are not old enough to legally drive.
+Or
+
+What is your age? 35
+You are old enough to legally drive.
+Constraints
+Use a single output statement.
+Use a ternary operator to write this program. If your language doesn’t support a ternary operator, use a regular
+if/else statement, and still use a single output statement to display the message.
+
+Challenges
+1- If the user enters a number that’s less than zero or enters non-numeric data, display an error message that asks
+the user to enter a valid age.
+Instead of hard-coding the driving age in your program logic, research driving ages for various countries
+and create a lookup table for the driving ages and countries. Prompt for the age, and display which countries
+the user can legally drive in.
+ */
+import java.util.Scanner;
+
+public class App {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("What is your age? ");
+        String stringAge = input.nextLine();
+        int age;
+
+        while (true) {
+            boolean stringChecker = isNum(stringAge);
+
+            if (stringChecker) {
+                age = Integer.parseInt(stringAge);
+                if (age > 0) {
+                    System.out.println((age >= 16) ? "You are old enough to legally drive" : "You are not old enough to legally drive.");
+                    System.exit(0);
+                } else {
+
+                    System.out.print("enter a valid age:");
+                    stringAge = input.nextLine();
+                    continue;
+                }
+            } else {
+                System.out.print("enter a valid age:");
+                stringAge = input.nextLine();
+                continue;
+            }
+        }
+
+    }
+
+
+
+    public static boolean isNum(String stringAge) {
+        if (stringAge == null) {
+            return false;
+        }
+        try {
+            int d = Integer.parseInt(stringAge);
+        } catch (NumberFormatException nfe) {
+            return false;
+        }
+        return true;
+    }
+
+}
+
